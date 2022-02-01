ff459cb495149975934ecc2a3dea1803c75739f5
jonathan-six <jonathan@six.network>
2022-02-01 18:50:02 +0700

    added TextEncoder

--- base.ts
+++ base.ts
@@ -1,3 +1,5 @@
+global.TextEncoder = require("util").TextEncoder;
+global.TextDecoder = require("util").TextDecoder;
 import axios from  "axios";
 import fs from "fs";
 import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
